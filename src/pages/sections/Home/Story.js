8c504eb05e39616c1f51782083c3b102268f0b77
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-10-15 11:25:30 -0700

    Removed emojis

--- src/pages/sections/Home/Story.js
+++ src/pages/sections/Home/Story.js
@@ -1,7 +1,7 @@
 function Story(){
     return(
         <section className="storySec" aria-label="Story Section">
-            <h1>Our Story 📚</h1>
+            <h1>Our Story</h1>
         </section>
     )
 }
