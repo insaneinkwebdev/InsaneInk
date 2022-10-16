2a9e3737f102a1089284b6028c92b86b0d9b6e7a
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-10-15 23:41:13 -0700

    Created skeleton for Portfolio sec

--- src/pages/sections/Home/Portfolio.js
+++ src/pages/sections/Home/Portfolio.js
@@ -1,7 +1,21 @@
+import portfolio from '../../../components/img/placeholderImg.jpg'
+import '../../Pages.css'
+
 function Portfolio (){
     return(
         <section className="portSec" aria-label="Portfolio Section">
-            <h1>Portfolio</h1>
+            <img className='port-content img' src={portfolio} alt='Placeholder'/>
+            <div className='port-content par'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies tortor nunc, 
+                sed scelerisque nisi feugiat vel. Sed sollicitudin neque id quam sollicitudin, eget s
+                agittis nisi auctor. Vivamus aliquet leo non sodales imperdiet. Sed hendrerit orci id 
+                facilisis aliquam
+
+                <br/>
+                <br/>
+
+                <a href="https://google.com">Learn More</a>
+            </div>
         </section>
     )
 }
