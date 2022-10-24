c057db9b832999cd54256b685021c80bac1c9aa0
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-10-24 11:07:32 -0700

    Edits + log

--- src/pages/sections/Home/Other.js
+++ src/pages/sections/Home/Other.js
@@ -3,12 +3,9 @@ import portfolio from '../../../components/img/placeholderImg.jpg'
 function Other (){
     return(
         <section className="otherSec" aria-label="Other Services Section">
-            <div className='other-content par'>
-                <img className='other-content img' src={portfolio} alt='Placeholder'/>
-            </div>
-            
-            <div className='other-content par'>
-                <img className='other-content img' src={portfolio} alt='Placeholder'/>
+            <h2 className='other-header'>Other Services</h2>
+            <div className='other-container'>
+               
             </div>
         </section>
     )
