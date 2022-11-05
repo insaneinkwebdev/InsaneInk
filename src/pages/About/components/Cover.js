7cb346cc7be4bb735c7f6156d067d26752e0a5f1
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-11-04 23:14:45 -0700

    mobile bug fix #2

--- src/pages/About/components/Cover.js
+++ src/pages/About/components/Cover.js
@@ -1,9 +1,11 @@
+import poster from "../../../images/about/poster.jpg"
+
 function Cover(){
     return(
         <>
             <section className="coverSec about" aria-label="Cover Screen">
                 <h2>About Our Company</h2>
-                <video controls playsInline autoPlay muted loop className="coverVid">
+                <video controls playsInline autoPlay muted loop poster={poster} className="coverVid">
                     <source src={require("../../../videos/2020-2021/2022Commercial.mp4")} type="video/ogg"/>
                     Your browser does not support the video tag.
                 </video>
