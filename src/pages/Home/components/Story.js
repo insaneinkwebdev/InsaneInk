cd94bd0cf9385d4770647c28920f422d4a4f1a46
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-11-07 12:06:53 -0800

    update year

--- src/pages/Home/components/Story.js
+++ src/pages/Home/components/Story.js
@@ -7,7 +7,7 @@ function Story(){
             <div className='story-content par'>
                 <h2 className='story-header'>About Us</h2>
                 
-                Founded in 1988, Insane Ink is an entrepreneurial course at Piedmont Hills High School where
+                Founded in 1998, Insane Ink is an entrepreneurial course at Piedmont Hills High School where
                 students are responsible for running an apparel customization company.
 
                 <br/>
