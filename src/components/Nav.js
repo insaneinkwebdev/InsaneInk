6c4d40209b9dcae5f6c4f1a77486ff936a7ab780
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-11-17 15:01:12 -0800

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -31,9 +31,8 @@ function Navbar(){
                     <li><a href="https://insaneinkv2.netlify.app/about">About Us</a></li>
                     <li><a href="https://www.insaneink.com/treasuretrove.php">Treasure Trove</a></li>
                     <li>
-                        <a href="https://www.insaneink.com/theprocess.php" className='drop-hover merch'>Order Now!</a>
+                        <a href="https://insaneinkv2.netlify.app/merchandmore" className='drop-hover merch'>Order Now!</a>
                         <ul className='drop merch'>
-                            <li><a href="https://www.insaneink.com/theprocess.php">How To Order</a></li>
                             <li><a href="https://www.insaneink.com/dtg.php">Small Quantity (DTG)</a></li>
                             <li><a href="https://www.insaneink.com/roland.php">Print Shop</a></li>
                             <li><a href="https://www.insaneink.com/roland.php">Heat Press</a></li>
