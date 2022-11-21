a2fcf952da8ca89467aed3be7ce628fb3819ea60
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-11-20 21:54:42 -0800

    tt

--- src/pages/TreasureTrove/TT.js
+++ src/pages/TreasureTrove/TT.js
@@ -1,5 +1,28 @@
 import "./TT.css"
 import dataTT from './dataTT'
+import placeholder from '../../images/placeholderImg.png'
+
+function openModal(id, index){
+    for(var i=0; i < dataTT.length; i++){
+        if(dataTT[i].id === id){
+            var modal = document.getElementById("modal");
+
+            let product = document.getElementById("modal__product");
+            let prodImg = document.getElementById("modal__img")
+            let price = document.getElementById("modal__price")
+            let desc = document.getElementById("modal__desc");
+
+            console.log(dataTT[i]);
+
+            product.innerHTML = dataTT[i].name;
+            price.innerHTML = dataTT[i].price;
+            desc.innerHTML = dataTT[i].description
+            prodImg.src = process.env.PUBLIC_URL + `/TT/product_${index + 1}.png`
+
+            modal.showModal(); 
+        }
+    }
+}
 
 function TT(){
     return(
@@ -11,7 +34,9 @@ function TT(){
                         console.log(obj, index)
                         return(
                             <div className="card stacked">
-                                <img src={process.env.PUBLIC_URL + `/TT/${index + 1}.png`} alt="product img" className="card__img"/>
+                                <img src={process.env.PUBLIC_URL + `/TT/product_${index + 1}.png`} alt="product img" className="card__img" onClick={function(){
+                                    openModal(obj.id, index)
+                                }}/>
                                 <div className="card__content">
                                     <h3 className="card__title">{obj.name}</h3>
                                     <p className="card__price">{obj.price}</p>
@@ -21,6 +46,15 @@ function TT(){
                     })}
                 </div>
             </div>
+
+            <dialog className="modal" id="modal">
+                <img id="modal__img" src={placeholder}></img>
+                <div className="modal__content">
+                    <h2 id="modal__product">Test modal</h2>
+                    <p id="modal__price">$999</p>
+                    <p id="modal__desc">This is sample text that should be dynamically changed on click. If you see this text... then oops</p>
+                </div>
+            </dialog>
         </div>
     );
 }
--- src/pages/TreasureTrove/dataTT.js
+++ src/pages/TreasureTrove/dataTT.js
@@ -4,55 +4,55 @@ const dataSlider = [
   {
     id: uuidv4(),
     name: "Test Product Not for Sale  #1",
-    price: "$0",
+    price: "$45",
     description: "This is sample product #1"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #2",
-    price: "$0",
+    price: "$20",
     description: "This is sample product #2"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #3",
-    price: "$0",
+    price: "$35",
     description: "This is sample product #3"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #4",
-    price: "$0",
+    price: "$237",
     description: "This is sample product #4"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #5",
-    price: "$0",
+    price: "$19",
     description: "This is sample product #5"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #6",
-    price: "$0",
+    price: "$9",
     description: "This is sample product #6"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #7",
-    price: "$0",
+    price: "$347",
     description: "This is sample product #7"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #8",
-    price: "$0",
+    price: "$18000",
     description: "This is sample product #8"
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #9",
-    price: "$0",
+    price: "$1220",
     description: "This is sample product #9"
   }
 ];
