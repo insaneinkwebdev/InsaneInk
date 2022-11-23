f91db3c30942a200c858531fa166a55aee3b6abd
Saa from Acedia <90130887+devintaan@users.noreply.github.com>
2022-11-22 20:41:35 -0800

    TT page finished!

--- src/pages/TreasureTrove/TT.js
+++ src/pages/TreasureTrove/TT.js
@@ -1,10 +1,16 @@
 import "./TT.css"
 import dataTT from './dataTT'
 import placeholder from '../../images/placeholderImg.png'
+import close from '../../images/close.svg'
+import facebook from '../../images/facebook.svg'
+import twitter from '../../images/twitter.svg'
 
 function openModal(id, index){
     for(var i=0; i < dataTT.length; i++){
         if(dataTT[i].id === id){
+            var sizeBoxContainer = document.getElementById("modal__size-container");
+            sizeBoxContainer.innerHTML = "";
+
             var modal = document.getElementById("modal");
 
             let product = document.getElementById("modal__product");
@@ -19,9 +25,50 @@ function openModal(id, index){
 
             prodImg.src = process.env.PUBLIC_URL + `/TT/product_${index + 1}/img_1.png`;
 
+            dataTT[i].sizes.map((obj) => {
+                var sbContainer = document.getElementById("modal__size-container");
+                let sizeBox = document.createElement("div");
+                let size = document.createElement("p");
+
+                size.innerHTML = obj.type; 
+                size.id = "modal__size-text"
+                sizeBox.appendChild(size);
+
+                sizeBox.id = "modal__size-box";
+                
+                if(obj.quant >= 5){
+                    sizeBox.className = "modal__size-box";
+                } else if (obj.quant !== 0 && obj.quant < 5){
+                    sizeBox.className = "modal__size-box low";
+                    sizeBox.onmouseover = showNotice;
+                    sizeBox.onmouseout = hideNotice;
+                } else {
+                    sizeBox.className = "modal__size-box oos";
+                }
+                
+                sbContainer.appendChild(sizeBox);
+
+                return(null)
+            })
+
             modal.showModal(); 
         }
     }
+}
+
+function closeModal(){
+    var modal = document.getElementById("modal");
+    modal.close();
+}
+
+function showNotice(){
+    var notice = document.getElementById("modal__inventory-notice-box");
+    notice.style.display = "block"
+}
+
+function hideNotice(){
+    var notice = document.getElementById("modal__inventory-notice-box");
+    notice.style.display = "none"
 }
 
 function TT(){
@@ -53,8 +100,26 @@ function TT(){
                         <h2 id="modal__product">Test modal</h2>
                         <p id="modal__price">$999</p>
                         <p id="modal__desc">This is sample text that should be dynamically changed on click. If you see this text... then oops</p>
+
+                        <div id="modal__inventory">
+                            <h3 id="modal__inventory-header">Inventory (as of 11/22)</h3>
+                            <div id="modal__inventory-notice-box" className="modal__inventory-notice-box">
+                                <p id="modal__inventory-notice" className="modal__inventory-notice">Notice: Less than 5 left in stock!</p>
+                            </div>
+                        </div>
+
+                        <div id="modal__sharebox-wrapper">
+                            <div id="modal__share-box" className="modal__share-box">
+                                <img className="modal__share-icon" src={twitter} alt="tweet product on twitter"></img>
+                            </div>
+                            <div id="modal__share-box" className="modal__share-box">
+                                <img className="modal__share-icon" src={facebook} alt="share product on facebook"></img>
+                            </div>
+                        </div>
                     </div>
                 </div>
+               
+                <img id="modal__close" src={close} onClick={closeModal} alt="close modal"></img>
             </dialog>
         </div>
     );
--- src/pages/TreasureTrove/dataTT.js
+++ src/pages/TreasureTrove/dataTT.js
@@ -6,126 +6,168 @@ const dataSlider = [
     name: "Test Product Not for Sale  #1",
     price: "$45.00",
     description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
+    sizes: [
+      {
+        "type": "XS",
+        "quant": 0
+      },
+      {
+        "type": "S",
+        "quant": 0
+      },
+      {
+        "type": "M",
+        "quant": 1
+      },
+      {
+        "type": "XL",
+        "quant": 8
+      },
+      {
+        "type": "2XL",
+        "quant": 0
+      }
+    ]
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #2",
     price: "$20.00",
     description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
+    sizes: [
+      {
+        "type": "XS",
+        "quant": 0
+      },
+      {
+        "type": "S",
+        "quant": 0
+      },
+      {
+        "type": "M",
+        "quant": 0
+      },
+      {
+        "type": "XL",
+        "quant": 4
+      },
+      {
+        "type": "2XL",
+        "quant": 0
+      }
+    ]
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #3",
     price: "$35.00",
     description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
+    sizes: [
+      {
+        "type": "XS",
+        "quant": 6
+      },
+      {
+        "type": "S",
+        "quant": 0
+      },
+      {
+        "type": "M",
+        "quant": 0
+      },
+      {
+        "type": "XL",
+        "quant": 0
+      },
+      {
+        "type": "2XL",
+        "quant": 0
+      }
+    ]
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #4",
     price: "$237.00",
     description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
+    sizes: [
+      {
+        "type": "XS",
+        "quant": 2
+      },
+      {
+        "type": "S",
+        "quant": 0
+      },
+      {
+        "type": "M",
+        "quant": 0
+      },
+      {
+        "type": "XL",
+        "quant": 0  
+      },
+      {
+        "type": "2XL",
+        "quant": 0
+      }
+    ]
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #5",
     price: "$19.00",
     description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
+    sizes: [
+      {
+        "type": "XS",
+        "quant": 5
+      },
+      {
+        "type": "S",
+        "quant": 10
+      },
+      {
+        "type": "M",
+        "quant": 3
+      },
+      {
+        "type": "XL",
+        "quant": 0
+      },
+      {
+        "type": "2XL",
+        "quant": 0
+      }
+    ]
   },
   {
     id: uuidv4(),
     name: "Test Product Not for Sale #6",
     price: "$9.00",
     description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
-  },
-  {
-    id: uuidv4(),
-    name: "Test Product Not for Sale #7",
-    price: "$347.00",
-    description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
-  },
-  {
-    id: uuidv4(),
-    name: "Test Product Not for Sale #8",
-    price: "$18000.00",
-    description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
-  },
-  {
-    id: uuidv4(),
-    name: "Test Product Not for Sale #9",
-    price: "$1220.00",
-    description: "Test description for a product, this will probably have some information about the garment, maybe color, material, idk.",
-    sizes: {
-      xsmall: 0,
-      small: 10,
-      medium: 10,
-      large: 5,
-      xlarge: 0,
-      txlarge: 0 
-    }
+    sizes: [
+      {
+        "type": "XS",
+        "quant": 4
+      },
+      {
+        "type": "S",
+        "quant": 3
+      },
+      {
+        "type": "M",
+        "quant": 0
+      },
+      {
+        "type": "XL",
+        "quant": 0
+      },
+      {
+        "type": "2XL",
+        "quant": 0
+      }
+    ]
   }
 ];
 
