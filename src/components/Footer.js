644304cb0ffa0f917b7a3ca65d3d8741cba77f3f
VeneziaM <137967766+VeneziaM@users.noreply.github.com>
2024-03-12 20:47:27 -0700

    fix footer
    its better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,7 +64,7 @@ function Footer(){
                     </div>
 
                     <div className='footer-row copyr'>
-                        &copy; 1998-2023 Insane Ink
+                        &copy; 1998-2024 Insane Ink
                     </div>
                   </div>
             </footer>
