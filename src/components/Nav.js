4d3be5d6824513744dbb0eb1160f007856155721
himanimanju <himanimanju05@gmail.com>
2024-05-16 23:42:11 -0700

    s

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -80,7 +80,7 @@ function Navbar(){
              {/*YEAR ROUND */}
             <div className='closedBanner'>
                 <p className='closedContent'>
-                    Due to being a student enterprise, Insane Ink Will be closed through August 2024.  We hope to serve your needs at that time.  Thank you for your support.
+                    Due to being a student enterprise, Insane Ink will be closed through August 2024.  We hope to serve your needs at that time.  Thank you for your support.
                 </p>
             </div>
         </>
