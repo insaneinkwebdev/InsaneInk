af1ea15aea4fa2ca0b90cbc6a6e8af4ae358bb5d
insaneinkwebdev <140704058+insaneinkwebdev@users.noreply.github.com>
2025-05-19 18:39:04 -0700

    Update Testimonials.js

--- src/pages/Testimonials/Testimonials.js
+++ src/pages/Testimonials/Testimonials.js
@@ -14,7 +14,54 @@ const testimonials = [
     name: "Elizabeth Caldera",
     quote: `The baseball shirt I ordered off them was great quality and hand delivered. Amazing people!`,
     images: [require('./Images/Caldera.jpg')],
-  }
+  },
+  {
+    name: "Ms. Wang",
+    quote: `Insane Ink is custom printing and apparel company run by students at Piedmont Hill High School. My experience as a customer has been overall positive and the quality of hoodie prints has always been excellent. The organization contributes a percentage of their income to projects that benefit the community and is overall a great opportunity for students get hands on experience on developing their customer relation and project management skills.
+
+Something to be aware of is that the prices are not always competitive and you have to give them ample time to deliver an order. Due to limited classroom space, Insane Ink doesn't keep a massive inventory of blank apparel or supplies to be competitive with large online warehouses. The employees are students so it is unfair to expect them to turn over a product overnight.
+
+If you are looking to work with a local printing business, please consider working with the organization. The students are always pleasant to work with eager to provide a positive experience with their customers and their representatives are always willing to negotiate in terms of timeline and pricing. 
+`,
+  },
+  {
+    name: "Solika M",
+    quote: `Two days before Christmas, I was able to gift my Secret Santa a thoughtful and heartfelt poster with pictures of her printed on high-quality, matte paper in a large size and low cost. I looked into a variety of printing services; however, my estimated cost usually ranged about $20, so I decided I would rather support Insane Ink, and I am really glad that I did. Not only was the poster much cheaper than alternative companies, the colors also came out wonderfully, and the ordering process was much easier than I initially imagined. For anyone looking to print anything, I would highly recommend looking into Insane Ink and always supporting small businesses.
+`,
+  },
+  {
+    name: "Hoai Le",
+    quote: `The crewnecks were extremely soft and the design came out perfectly. I will be buying from Insane Ink in the future.
+`,
+  },
+  {
+    name: "Karen Ho",
+    quote: `From start to finish, the communication was great. The team kept me updated every step of the way and made the entire process super smooth. You can really tell they care about their customers and take pride in their work.
+What makes it even cooler is that Insane Ink is a student-run enterprise! It’s inspiring to see students gaining real-world, hands-on experience in design, customer service, and business operations. They were professional, creative, and so easy to work with.
+The jackets we ordered turned out so lovely — the design was clean, vibrant, and exactly what we envisioned. If you’re looking to support student talent while getting high-quality custom apparel, I highly recommend Insane Ink! 
+`,
+  },
+  {
+    name: "Connie Liang",
+    quote: `Insane ink has great quality prints; the colors are accurate, the print doesn't feel cheap + it's long lasting, and the shirts are of good material. They're also very reliable and have fast communication, and the prices are reasonable. Overall, they're a great option for those looking for shirt-printing services!
+`,
+  },
+  {
+    name: "Maggie Chen",
+    quote: `My friends and I bought matching shirts for Christmas party. The design is adorable and quality of the shirts were soft. They were also very affordable compared to Etsy and other sites. 
+`,
+  },
+  {
+    name: "Danju Kuo",
+    quote: `Super comfy and easy to choose when you’re too lazy to pick an outfit. It's stylish and goes well with anything! 😀
+`,
+  },
+  {
+    name: "Rochelle Barbiran",
+    quote: `Love our insane ink Christmas sweatshirts. It is great, soft, warm, not too thick or thin! Would definitely purchase again. Great job to Insane Ink.
+`,
+  },
+
 ];
 
 const Testimonials = () => {
